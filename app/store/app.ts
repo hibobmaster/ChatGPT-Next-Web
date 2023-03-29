c9f4e22095bbd726f61763963f2e01fddd1b14b9
hibobmaster <32976627+hibobmaster@users.noreply.github.com>
2023-03-29 23:58:53 +0800

    Update default chat configs

--- app/store/app.ts
+++ app/store/app.ts
@@ -131,8 +131,8 @@ const DEFAULT_CONFIG: ChatConfig = {
 
   modelConfig: {
     model: "gpt-3.5-turbo",
-    temperature: 1,
-    max_tokens: 2000,
+    temperature: 0.8,
+    max_tokens: 4096,
     presence_penalty: 0,
   },
 };
